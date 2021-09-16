8a1cbc86fae60ad48e5e1085b1c2ef3dc2b66849
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-09-16 12:16:48 -0700

    Thursday12:15PST

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,35 @@
 class GameController < ApplicationController
 
-        get "/games" do
+        get '/games' do
             Game.all.to_json
             #{we_in: "game"}.to_json
         end
-    
+        
+        get "/games/:id" do
+            puts"In: get'/games/id'"
+
+                g = Game.find(params[:id])
+
+                game_for_front_end = u.to_json
+
+                game_for_front_end
+        end
+
+        post '/games' do
+            Game.create.to_json
+        end
+
+        delete '/games/:id' do
+            game = Game.find(params[:id])
+            game.destroy_all
+            #game.to_json
+        end
+
+        private
+
+            def game_params
+                allowed_params=%w(game_name game_description game_image)
+                params.select{|k,v| allowed_params.include?(k)}
+            end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,23 @@ class UserController < ApplicationController
         User.all.to_json
         #{ we_in: "user"}.to_json
     end
+
+    get "/users/:id" do
+        #{ we_in: "user"}.to_json
+        puts"In: get'/users/id'"
+
+            u = User.find(params[:id])
+
+            user_for_front_end = u.to_json
+
+            user_for_front_end
+    end
+
+    delete "/users/:id" do
+        user_to_delete = User.find(params[:id])
+        user_to_delete.destroy
+    end
+
+
+
 end
